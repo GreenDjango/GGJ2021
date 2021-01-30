de3b1f19ab176f5b21aaa13c01860e50cf45d9fa
Killian DROULEZ <killian.droulez@outlook.fr>
2021-01-30 23:30:15 +0100

    Ajout de la orca.tscn et orca.gd

---
+++ src/scripts/orca.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+export(NodePath) var talking_zone_path
+var talking_zone: Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	talking_zone = get_node(talking_zone_path)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
