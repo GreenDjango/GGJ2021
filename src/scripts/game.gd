55eac03ad223597e55ae8a2b074f42ad90c01263
Theo <52924057+GreenDjango@users.noreply.github.com>
2021-01-31 16:17:00 +0100

    FOOOOOOOCK
    Co-Authored-By: Paquity <42215723+Paquity@users.noreply.github.com>

--- src/scripts/game.gd
+++ src/scripts/game.gd
@@ -3,12 +3,31 @@ extends Node2D
 func _ready():
 	pass
 
+func _switch_blizzard(active: bool):
+	var animator_group : Array = get_tree().get_nodes_in_group("animator")
+	if !animator_group || animator_group.empty():
+		return
+	var animator_node : AnimationPlayer = animator_group[0]
+	if active:
+		animator_node.play("blizzard")
+	else:
+		animator_node.play_backwards("blizzard")
+
+func _is_player(body : Node ) -> bool: 
+	return body is KinematicBody2D && body.is_in_group("player")
 
 func _on_StartArea_body_entered(body):
-	if body is KinematicBody2D && body.is_in_group("player"):
+	if _is_player(body):
 		Globals.dialog = "You need to find your egg!"
 
-
 func _on_StartArea_body_exited(body):
-	if body is KinematicBody2D && body.is_in_group("player"):
+	if _is_player(body):
 		Globals.dialog = ""
+
+func _on_BlizzardArea_body_entered(body):
+	if _is_player(body):
+		_switch_blizzard(true)
+
+func _on_BlizzardArea_body_exited(body):
+	if _is_player(body):
+		_switch_blizzard(false)
